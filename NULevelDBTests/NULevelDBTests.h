09cc62247b056463471f1f360fedfedb7b5cdf26
Brent Gulanowski <brent.gulanowski@nulayer.com>
2011-08-04 12:09:09 -0400

    Add new iOS Framework project.

---
+++ NULevelDBTests/NULevelDBTests.h
@@ -0,0 +1,13 @@
+//
+//  NULevelDBTests.h
+//  NULevelDBTests
+//
+//  Created by Brent Gulanowski on 11-08-04.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NULevelDBTests : SenTestCase
+
+@end
