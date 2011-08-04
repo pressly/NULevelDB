6c672129e4848ceb0c879fb01ece5bf3c1fd03e9
Brent Gulanowski <brent.gulanowski@nulayer.com>
2011-08-04 15:24:31 -0400

    Implement function body.

--- Tests/NULDBTestUtilities.m
+++ Tests/NULDBTestUtilities.m
@@ -10,7 +10,17 @@
 
 
 NSString *NULDBRandomName( void ) {
-    return nil;
+    
+    static char buffer[256];
+    int length = Random_int_in_range(2, 40);
+    
+    for(int i=0; i<length; ++i)
+        buffer[i] = Random_alpha_char();
+    
+    buffer[0] -= 0x20;
+    buffer[length] = '\0';
+    
+    return [NSString stringWithCString:buffer encoding:NSASCIIStringEncoding];
 }
 
 @implementation NULDBTestUtilities
