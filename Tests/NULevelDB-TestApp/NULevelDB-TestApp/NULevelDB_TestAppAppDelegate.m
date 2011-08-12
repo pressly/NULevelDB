a63c211b763f27b2f870ca03d11b44c8dfa5d201
Brent Gulanowski <brent.gulanowski@nulayer.com>
2011-08-12 12:46:29 -0400

    Full suite of timed tests for the bulk interfaces.

--- Tests/NULevelDB-TestApp/NULevelDB-TestApp/NULevelDB_TestAppAppDelegate.m
+++ Tests/NULevelDB-TestApp/NULevelDB-TestApp/NULevelDB_TestAppAppDelegate.m
@@ -14,6 +14,8 @@
 #import "NULDBTestUtilities.h"
 
 #import "NULDBTestCompany.h"
+#import "NULDBTestAddress.h"
+#import "NULDBTestPerson.h"
 
 
 @interface NULDBDB (Tests)
@@ -116,6 +118,89 @@
 
 @implementation NULDBDB (Tests)
 
+- (NSData *)makeTestData {
+    
+    static NSData *data;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+        NSError *error = nil;
+        data = [NSPropertyListSerialization dataWithPropertyList:[[NULDBTestAddress randomAddress] plistRepresentation]
+                                                          format:NSPropertyListBinaryFormat_v1_0
+                                                         options:0
+                                                           error:&error];
+        if(nil == data)
+            NSLog(@"Failed to make data; %@", error);
+    });
+    
+    return data;
+}
+
+enum {
+    kGeneric,
+    kData,
+    kString
+};
+
+#if TARGET_IPHONE_SIMULATOR
+#define test_count 10000
+#else
+#define test_count 1000
+#endif
+
+- (NSDictionary *)makeTestDictionary:(unsigned)type {
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:test_count];
+    
+    for(int i = 0; i<test_count; ++i) {
+        
+        NULDBTestPerson *person = [NULDBTestPerson randomPerson];
+        NSDictionary *plist = [person plistRepresentation];
+        NSData *plistData = nil;
+        id key = [person uniqueID];
+        id value;
+        
+        if(type > 0) {
+            int plistType = NSPropertyListBinaryFormat_v1_0;
+            if(type > 1)
+                plistType = NSPropertyListXMLFormat_v1_0;
+            plistData = [NSPropertyListSerialization dataWithPropertyList:plist format:plistType options:0 error:NULL];
+        }
+        
+        switch (type) {
+                
+            case kString:
+                value = [[NSString alloc] initWithData:plistData encoding:NSUTF8StringEncoding];
+                break;
+                
+            case kData:
+                value = plistData;
+                key = [key dataUsingEncoding:NSUTF8StringEncoding];
+                break;
+                
+            case kGeneric:
+            default:
+                value = plist;
+                break;
+        }
+        
+        [dict setObject:value forKey:key];
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:dict];
+}
+
+typedef struct testResult {
+    BOOL failed;
+    NSUInteger count;
+    NSUInteger loadCount;
+    NSTimeInterval store;
+    NSTimeInterval load;
+    NSTimeInterval delete;
+} TestResult;
+
+
 // a^2 is the number of keys, b^2 is the size
 // a counts up by powers of 2 and by counts down by powers of 2
 - (void)testPowersOf2FromA:(NSUInteger)a toB:(NSUInteger)b lowerLimit:(NSUInteger)l {
@@ -214,6 +299,190 @@
     NSLog(@"Finished deleting. Took %0.4f seconds. Done testing", end - start);
 }
 
+- (BOOL)runBulkGenericTests:(TestResult *)testResult {
+    
+    NSTimeInterval start, end;
+    NSDictionary *testData = [self makeTestDictionary:kGeneric];
+    
+    testResult->count = [testData count];
+    
+    start = [NSDate timeIntervalSinceReferenceDate];
+    [self storeValuesFromDictionary:testData];
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->store = end - start;
+    
+    start = end;
+    NSDictionary *copy = [self storedValuesForKeys:[testData allKeys]];
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->loadCount = [copy count];
+    testResult->load = end-start;
+    
+    start = end;
+    [self deleteStoredValuesForKeys:[testData allKeys]];
+    testResult->delete = [NSDate timeIntervalSinceReferenceDate]-start;
+    
+    return YES;
+}
+
+- (BOOL)runBulkDataTests:(TestResult *)testResult {
+    
+    NSError *error = nil;
+    NSTimeInterval start, end;
+    NSDictionary *testData = [self makeTestDictionary:kData];
+    
+    testResult->count = [testData count];
+    
+    start = [NSDate timeIntervalSinceReferenceDate];
+    if(![self storeDataFromDictionary:testData error:&error]) {
+        NSLog(@"Error storing; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->store = end - start;
+    start = end;
+    
+
+    NSDictionary *copy = [self storedDataForKeys:[testData allKeys] error:&error];
+
+    end = [NSDate timeIntervalSinceReferenceDate];
+    if (nil == copy) {
+        NSLog(@"Error loading; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    testResult->loadCount = [copy count];
+    testResult->load = end - start;
+
+    start = end;
+    if(![self deleteStoredDataForKeys:[testData allKeys] error:&error]) {
+        NSLog(@"Error deleting; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    testResult->delete = [NSDate timeIntervalSinceReferenceDate]-start;
+    
+    return YES;
+}
+
+- (BOOL)runBulkStringTests:(TestResult *)testResult {
+    
+    NSError *error = nil;
+    NSTimeInterval start, end;
+    NSDictionary *testData = [self makeTestDictionary:kString];
+    
+    testResult->count = [testData count];
+    
+    start = [NSDate timeIntervalSinceReferenceDate];
+    if(![self storeStringsFromDictionary:testData error:&error]) {
+        NSLog(@"Error storing; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->store = end-start;    
+    start = end;
+    
+    
+    NSDictionary *copy = [self storedStringsForKeys:[testData allKeys] error:&error];
+    
+    if (nil == copy) {
+        NSLog(@"Error loading; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->loadCount = [copy count];
+    testResult->load=end-start;
+    
+    start = end;
+    if(![self deleteStoredStringsForKeys:[testData allKeys] error:&error]) {
+        NSLog(@"Error deleting; %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    testResult->delete = [NSDate timeIntervalSinceReferenceDate]-start;
+    
+    return YES;
+}
+
+- (BOOL)runBulkIndexTests:(TestResult *)testResult {
+    
+    NSError *error = nil;
+    NSTimeInterval start, end;
+    NSDictionary *testData = [self makeTestDictionary:kData];
+    NSUInteger count = [testData count];
+    
+    uint64_t *indices = malloc(sizeof(uint64_t)*count);
+    
+    for(NSUInteger i=0; i<count; ++i) {
+        indices[i] = (uint64_t)random() << 32 | i;
+    }
+    
+    testResult->count = count;
+    
+    start = [NSDate timeIntervalSinceReferenceDate];
+    if(![self storeDataFromArray:[testData allValues] forIndexes:indices error:&error]) {
+        NSLog(@"Error storing for bulk indices: %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->store = end-start;
+    
+    start = end;
+    NSArray *copy = [self storedDataForIndexes:indices count:count error:&error];
+    if(nil == copy) {
+        NSLog(@"Error loading for bulk indices;: %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    end = [NSDate timeIntervalSinceReferenceDate];
+    testResult->loadCount = [copy count];
+    testResult->load = end-start;
+    
+    start = end;
+    if(![self deleteStoredDataForIndexes:indices count:count error:&error]) {
+        NSLog(@"Error deleting for bulk indices: %@", error);
+        testResult->failed = YES;
+        return NO;
+    }
+    testResult->delete = [NSDate timeIntervalSinceReferenceDate] - start;
+    
+    return YES;
+}
+
+- (void)runBulkOperationTests {
+    
+    TestResult testResult[4] = { {}, {}, {}, {} };
+    
+    NSLog(@"Starting bulk tests. Generic...");
+        
+    [self runBulkGenericTests:testResult];
+    NSLog(@"...data...");
+    [self runBulkDataTests:testResult+1];
+    NSLog(@"...string...");
+    [self runBulkStringTests:testResult+2];
+    NSLog(@"...index...");
+    [self runBulkIndexTests:testResult+3];
+    
+    TestResult result;
+    
+    NSArray *names = [NSArray arrayWithObjects:@"generic", @"data", @"string", @"index", nil];
+    
+    printf("\ntest results: Count   Store       per    Load  (count)       per  Delete     per\n\n");
+
+    NSUInteger i=0;
+    for(NSString *test in names) {
+        result = testResult[i++];
+        if(result.failed)
+            printf(" %11s:  TEST FAILED", [test UTF8String]);
+        else
+            printf(" %11s: %5u %03.5f %01.7f %03.5f %7u  %01.7f %3.5f %01.7f\n",
+                   [test UTF8String], result.count, result.store, result.store/result.count, result.load, result.loadCount, result.load/result.count, result.delete, result.delete/result.count);
+    }
+}
+
 - (void)runTests {
     
 //    [self run4By8Tests];
@@ -222,8 +491,9 @@
     
 //    [self run4By14Tests];
     
-    [self runGraphTests];
+//    [self runGraphTests];
     
+    [self runBulkOperationTests];
     
     NSLog(@"Testing finished");
 }
