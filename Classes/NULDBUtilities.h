b9f9dff2905842f912c1c5e2b0a48eb15dd419f2
Brent Gulanowski <brent.gulanowski@nulayer.com>
2011-11-04 15:35:19 -0400

    Partition NULDBDB into multiple implementation files.

--- Classes/NULDBUtilities.h
+++ Classes/NULDBUtilities.h
@@ -17,6 +17,13 @@ using namespace leveldb;
 #define NULDBLog(frmt, ...) do{ if(logging) NSLog((frmt), ##__VA_ARGS__); } while(0)
 
 
+#define NULDBSliceFromData(_data_) (Slice((char *)[_data_ bytes], [_data_ length]))
+#define NULDBDataFromSlice(_slice_) ([NSData dataWithBytes:_slice_.data() length:_slice_.size()])
+
+#define NULDBSliceFromString(_string_) (Slice((char *)[_string_ UTF8String], [_string_ lengthOfBytesUsingEncoding:NSUTF8StringEncoding]))
+#define NULDBStringFromSlice(_slice_) ([[[NSString alloc] initWithBytes:_slice_.data() length:_slice_.size() encoding:NSUTF8StringEncoding] autorelease])
+
+
 extern Class stringClass;
 extern Class dataClass;
 extern Class dictClass;
