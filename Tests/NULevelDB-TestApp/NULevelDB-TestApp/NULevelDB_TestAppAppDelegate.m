03de8bb21b0f32545146e5a8c461d93ebe598a10
Brent Gulanowski <brent.gulanowski@nulayer.com>
2011-11-09 14:56:02 -0500

    Tidy up.

--- Tests/NULevelDB-TestApp/NULevelDB-TestApp/NULevelDB_TestAppAppDelegate.m
+++ Tests/NULevelDB-TestApp/NULevelDB-TestApp/NULevelDB_TestAppAppDelegate.m
@@ -63,45 +63,6 @@
     NSLog(@"Company: %@", [company plistRepresentation]);
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
-}
-
 - (void)runTests {
     
     NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
@@ -110,7 +71,7 @@
 //    NSString *resultsPath = [docPath stringByAppendingPathComponent:@"results.storage"];
     
     if([[NSFileManager defaultManager] removeItemAtPath:testPath error:NULL])
-        NSLog(@"Deleted previous test db");
+        NSLog(@"Deleted previous test db.");
     
 
     NULDBDB *db = [[NULDBDB alloc] initWithLocation:testPath];
@@ -122,9 +83,8 @@
     
     const int testCount = 100;
     
-//    [tester runCompanyTest:testCount];
-//    [tester runBigTest:testCount];
     [tester runFineGrainedTests:testCount];
+
     NSLog(@"Results: %@", [tester resultsTableString]);
     
 #else
